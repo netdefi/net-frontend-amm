a3c97835dbd6ecee9b4c979816832167c0ea7355
NETAdmin <netadmin.bsc@gmail.com>
2021-03-17 12:57:45 +0700

    exchange

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,96 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://www.netdefi.com/'
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap'
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool'
+      }
+    ]
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://www.netdefi.com/farms'
+  },
+  {
+    label: 'Nests',
+    icon: 'PoolIcon',
+    href: 'https://www.netdefi.com/nests'
+  },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: 'https://www.netdefi.com/lottery'
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+      },
+      {
+        label: 'CoinGecko',
+        href: 'https://www.coingecko.com/en/coins/net-finance',
+      },
+      {
+        label: 'CoinMarketCap',
+        href: 'https://coinmarketcap.com/currencies/net-finance/',
+      },
+      {
+        label: 'AstroTools',
+        href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
+      },
+    ]
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: "Github",
+        href: "https://github.com/netdefi/",
+      },
+      {
+        label: "Docs",
+        href: "https://netdefi.gitbook.io/net-finance/",
+      },
+      {
+        label: "Blog",
+        href: "https://netfinance.medium.com/",
+      },
+    ],
+  },
+  {
+    label: 'Partnerships/IFO',
+    icon: 'GooseIcon',
+    href: 'https://docs.google.com/forms/d/e/1FAIpQLSe7ycrw8Dq4C5Vjc9WNlRtTxEhFDB1Ny6jlAByZ2Y6qBo7SKg/viewform?usp=sf_link',
+  },
+  {
+    label: 'Audit by Hacken',
+    icon: 'AuditIcon',
+    href: 'https://www.netdefi.com/files/hackenAudit.pdf',
+  },
+  {
+    label: 'Audit by CertiK',
+    icon: 'AuditIcon',
+    href: 'https://certik.org/projects/net-finance',
+  },
+]
+
+export default config
