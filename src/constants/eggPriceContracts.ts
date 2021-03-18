6c9735b69ab0e04d318434b54a6da78c53b3565b
NETAdmin <netadmin.bsc@gmail.com>
2021-03-18 13:38:06 +0700

    exchange

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
+  cakeAddress: '0x2CC6a05e1729e568f69AC3f52Ee89d40747239D2',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
+  lpAddress: '0xEB33F0F47b015A59e2a11ef76bc033D2Da096c3E'
 }
 
 export default priceContracts
